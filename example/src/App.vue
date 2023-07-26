865613d4783be27eebe30b32f6cb113b257c5ffe
l1ng1 <pk9942601@gmail.com>
2023-07-26 15:14:15 +0600

    done

---
+++ example/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+    здесь будет мой текст на английсом => <MyCompoment></MyCompoment>
+</template>
+
+<script>
+import MyCompoment from './components/myCompoment.vue';
+
+export default {
+  name: 'App',
+  components: {
+    MyCompoment
+  }
+}
+</script>
+
---
+++ example/src/components/myCompoment.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="root">
+        
+
+        <button  v-on:click="minus" >-</button>
+        <input  v-model="inputValue" type="number" max="10" min="1">
+        <button  v-on:click="plus" >+</button>
+
+
+
+
+        <!-- <input type="text" placeholder="Ваше имя" @input="getNameValue($event.target.value)">
+        <input type="text" placeholder="Ваша фамилия" @input="getLNameValue($event.target.value)">
+        <input type="text" placeholder="Ваш возраст" @input="getAgeValue($event.target.value)">
+
+
+        <h2>Меня зовут:{{ firstName }}</h2>
+        <h2>Моя фамилия:{{ lastName }}</h2>
+        <h2>Мне:{{ age }}</h2> -->
+
+
+
+    </div>
+</template>
+
+
+<script>
+export default {
+    name:'MyCompoment',
+    data() {
+        return{
+            // firstName:'',
+            // lastName:'',
+            // age:'',
+            inputValue:1,
+        }
+    },
+    methods:{
+        // getNameValue(event){this.firstName = event},
+        // getLNameValue(event){this.lastName = event},
+        // getAgeValue(event){this.age = event},
+
+        minus(){
+            if(this.inputValue<2) return;
+            this.inputValue -= 1;
+        },
+        plus(){
+            if(this.inputValue>9) return;
+            this.inputValue +=1;
+        },
+    }
+    
+}
+
+
+</script>
