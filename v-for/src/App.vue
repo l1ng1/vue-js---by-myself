eb346030941f0189637e52266a93cae9c6ad9520
l1ng1 <pk9942601@gmail.com>
2023-07-27 20:45:24 +0600

    вторая домашка дан

---
+++ v-for/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  
+  <searchNames/>
+</template>
+
+<script>
+import searchNames from './components/searchNames.vue'
+
+export default {
+  name: 'App',
+  components: {
+    searchNames
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ v-for/src/components/searchNames.vue
@@ -0,0 +1,72 @@
+<template>
+  
+  {{ blablabla() }}
+  <h2  >Поиск имен</h2>
+  <br>
+  <input v-on:input="serchNames($event.target.value)" type="text" placeholder="имя">
+
+  {{ showMainString }}
+  {{ showLength }}
+  <br>
+  <ul>
+    <li v-for="item in namesToShoe" :key="item">
+      {{ item }}
+    </li>
+  </ul>
+
+
+
+</template>
+
+<script>
+export default {
+  name: 'searchNames',
+  data(){
+    return{
+      namesToShoe:[],
+      names:[
+        {fName:'Петя'},
+        {fName:'Вова'},
+        {fName:'Коля'},
+        {fName:'Петя'},
+        {fName:'Рома'},
+        {fName:'Вова'},
+      ],
+      totalnames:0,
+      sameNames:0,
+    }
+    
+  },
+
+  computed:{
+    showMainString(){
+      return `Всего имен: ${this.totalnames}. Совпадений : ${this.sameNames}`
+    },
+    
+  },
+  methods:{
+      blablabla(){
+        this.totalnames = this.names.length;
+      },
+      serchNames(name){
+        this.namesToShoe.length = 0;
+        this.sameNames =0;
+        for(let x of this.names){
+          if(x.fName.toLocaleUpperCase() == name.toLocaleUpperCase()) {
+            this.namesToShoe.push(x.fName);
+            this.sameNames++;
+          }
+        }
+        console.log(JSON.stringify( this.namesToShoe));
+    }
+  }
+  
+}
+</script>
+
+
+<style scoped>
+
+
+
+</style>
